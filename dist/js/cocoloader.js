d00527e47776aac865c2fa8a6c8131e94227b74a
VladScv <ddcllrnsbrv@uoc.edu>
2022-10-28 13:14:34 +0200

    redesigning all paths

--- dist/js/cocoloader.js
+++ dist/js/cocoloader.js
@@ -11,11 +11,24 @@ window.onresize=function (){
     resizeTimer=setTimeout(function (){cocoResize()},100);
 }
 document.onload= function (){
-
+    cocoResize();
 
 }
 
 
 function cocoResize() {
-   //TODO
+   // let mainWidth= visualViewport.width;
+   // let mainHeight= visualViewport.height;
+   // let isVertical= mainWidth<mainHeight;
+   // let vw=(mainWidth/100);
+   // let columns = Math.round(mainWidth / (180+ (vw*2)));
+   // let rest = (mainWidth / columns)-250;
+   // const cards = document.querySelectorAll('.card');
+   //
+   //  cards.forEach(card => {
+   //      card.setAttribute('width', (rest>2*vw)?'250px':Math.round(mainWidth/columns)+'px');
+   //      card.style.margin= rest>2*vw? Math.round(rest)+'px': (2*vw)+'px';
+   //  });
+
+
 }
