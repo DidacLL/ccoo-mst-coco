7ceff7e461c8a596bdb65746127be2163011333e
VladScv <ddcllrnsbrv@uoc.edu>
2022-12-07 21:02:18 +0100

    editMode adding content

--- dist/js/cocoloader.js
+++ dist/js/cocoloader.js
@@ -1,7 +1,66 @@
 //----------------------------------------------------------------------------------------------------- Global Variables
 import {ContentLoader} from "./content.js";
 import {Card} from "./content.js";
+import {PAGES} from "./content.js";
 
+const COCODATA_PATH = '../dist/cocodata.json';
+
+const EDITOR_HTML = `
+<div id= "editor-header" class="m-5 no-wrap">
+    <h2>Admin Mode</h2>
+    <button id="btn-save" class="btn-primary px-3 py-1 mx-3 disabled"> Save </button>
+    <button id="btn-cancel" class="btn-primary px-3 py-1 mx-3"> Exit </button>
+    <button id="btn-help" class="btn-primary px-3 py-1 mx-3"> Help </button>
+</div>
+<form class="m-5">
+    <label for="section-select"> Selecciona la sección </label>
+    <select id="section-select" class="form-control sm-10 m-3">
+        <option selected value="-1"> elige...</option>
+                <option value="0">Home</option>
+                <option value="1">Comunicados</option>
+                <option value="2">Documentos</option>
+                <option value="3">Magazines</option>
+                <option value="4">Pildoras</option>
+                <option value="5">Plantillas</option>
+                <option value="6">Apps</option>
+                <option value="7">Sobre nosotros</option>
+            </select>
+    <label for="type-select"> Selecciona el tipo </label>
+    <select id="type-select" class="form-control sm-10 m-3">
+        <option selected value="0"> elige...</option>
+        <option value="1">Article</option>
+        <option value="2">Link</option>
+        <option value="3">Shortcut</option>
+        
+    </select>
+    <div class="collapse " id="editOptions">
+        <p class="label row no-wrap m-2" >Title</p>
+        <div class="form-outline row no-wrap m-2">
+            <input type="text" id="card-title" class="form-control col-sm-10 m-2" data-mdb-showcounter="true"
+                   maxlength="35"
+                   placeholder="TITULO"/>
+            <label class="label label-default mt-3" id="count_title" for="card-title">Title</label>
+        </div>
+        <p class="label row no-wrap m-2" >Description</p>
+        <div class="form-outline row no-wrap m-2">
+            <input type="text" id="card-description" class="form-control col-sm-10 m-2" data-mdb-showcounter="true"
+                   maxlength="35" placeholder="DESCRIPCION breve para la previsualización"/>
+            <label class="label label-default mt-3" id="count_description" for="card-description"></label>
+        </div>
+        <div id="content-box">
+        </div>
+        <p class="label row no-wrap m-2" >Header Picture</p>
+        <div class="form-outline row no-wrap m-2">
+            <input type="text" id="card-picture" class="form-control col-sm-10 m-2"
+                   placeholder="IMAGEN Introducir nombre del archivo alojado en './dist/img' o URL"/>
+        </div>
+    </div>
+    <div class="form-outline row no-wrap m-2" id="preview"></div>
+</form>
+<div class=" footer mt-auto py-3 ">
+    <button id="btn-process" class="btn-primary p-3 m-3 disabled"> Previsualizar</button>
+    <button id="btn-generate" class="btn-primary p-3 m-3"> Añadir</button>
+</div>`;
 
 let resizeTimer;
 let currentContent;
@@ -18,10 +77,6 @@ window.onresize = function () {
     resizeTimer = setTimeout(function () {
         cocoResize()
     }, 100);
-}
-
-function loadHome() {
-
 }
 
 function cleanAllContent() {
@@ -72,94 +127,40 @@ function loadFile(path, elem) {
         }
         xhttp.open("GET", path, true);
         xhttp.send();
-        return;
     }
 }
 
-function getFile(path) {
-    let xhttp;
-    let res;
-    if (path) {
-        xhttp = new XMLHttpRequest();
-        xhttp.onreadystatechange = function () {
-            if (this.readyState === 4) {
-                if (this.status === 200) {
-                    contentVault = new ContentLoader(this.responseText);
-                } else {
-                    contentVault = new ContentLoader(null);
-                }
+function loadCocoData() {
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            if (this.status === 200) {
+
+                console.log('COCO-DATA.JSON loaded!!')
+                contentVault = new ContentLoader(this.responseText);
+                console.log(contentVault)
+            } else {
+                console.log('Failed to load COCO-DATA.JSON')
+                contentVault = new ContentLoader(null);
             }
         }
-        xhttp.open("GET", path, true);
-        xhttp.send();
-
-        return res;
     }
+    xhttp.open("GET", COCODATA_PATH, true);
+    xhttp.send();
 }
 
 function closeEditMode() {
     removeAllChildNodes(document.querySelector('#blank_space'))
-    document.body.style.overflow='hidden';
+    document.body.style.overflow = 'hidden';
     editMode = false;
 }
 
-function addNewContent() {
-    if(editMode){
+function openEditMode() {
+    if (editMode) {
         closeEditMode();
-        addNewContent();
-    }else {
-        document.querySelector('#blank_space').insertAdjacentHTML('afterbegin', `
-        <div class="flex-row m-5">
-            <h2>Rellena los campos para crear el nuevo contenido.</h2>
-        </div>
-        <form class="m-5">
-            <label for="section-select"> Selecciona la sección </label>
-            <select id="section-select" class="form-control sm-10 m-3">
-                <option selected value="0"> elige...</option>
-                <option value="1">Home</option>
-                <option value="2">Comunicados</option>
-                <option value="3">Documentos</option>
-                <option value="4">Magazines</option>
-                <option value="5">Pildoras</option>
-                <option value="6">Plantillas</option>
-                <option value="7">Apps</option>
-                <option value="8">Sobre nosotros</option>
-            </select>
-            <label for="type-select"> Selecciona el tipo </label>
-            <select id="type-select" class="form-control sm-10 m-3">
-                <option selected value="0"> elige...</option>
-                <option value="1">Article</option>
-                <option value="2">Link</option>
-            </select>
-            <div class="collapse " id="editOptions">
-                <p class="label row no-wrap m-2" >Title</p>
-                <div class="form-outline row no-wrap m-2">
-                    <input type="text" id="card-title" class="form-control col-sm-10 m-2" data-mdb-showcounter="true"
-                           maxlength="35"
-                           placeholder="TITULO"/>
-                    <label class="label label-default mt-3" id="count_title" for="card-title">Title</label>
-                </div>
-                <p class="label row no-wrap m-2" >Description</p>
-                <div class="form-outline row no-wrap m-2">
-                    <input type="text" id="card-description" class="form-control col-sm-10 m-2" data-mdb-showcounter="true"
-                           maxlength="35" placeholder="DESCRIPCION breve para la previsualización"/>
-                    <label class="label label-default mt-3" id="count_description" for="card-description"></label>
-                </div>
-                <div id="content-box">
-                </div>
-                <p class="label row no-wrap m-2" >Header Picture</p>
-                <div class="form-outline row no-wrap m-2">
-                    <input type="text" id="card-picture" class="form-control col-sm-10 m-2"
-                           placeholder="IMAGEN Introducir nombre del archivo alojado en './dist/img' o URL"/>
-                </div>
-            </div>
-            <div class="form-outline row no-wrap m-2" id="preview"></div>
-        </form>
-        <div class=" footer mt-auto py-3 ">
-            <button id="btn-process" class="btn-primary p-3 m-3 disabled"> Previsualizar</button>
-            <button id="btn-generate" class="btn-primary p-3 m-3"> Añadir</button>
-            <button id="btn-cancel" class="btn-primary p-3 m-3"> Cancelar</button>
-        </div>`);
+        openEditMode();
+    } else {
+        document.querySelector('#blank_space').insertAdjacentHTML('afterbegin', EDITOR_HTML);
         editMode = true;
         createForm();
         checkAllInputs();
@@ -169,21 +170,23 @@ function addNewContent() {
 }
 
 function changeMenu() {
-    if(editMode)closeEditMode();
+    if (editMode) closeEditMode();
+}
+
+function setMenuListeners() {
+    document.querySelector('#menu-publish-btn').addEventListener('click', () => openEditMode());
+    document.querySelectorAll('.dropdown-item')
+        .forEach(item => item.addEventListener('click', () => item !== document.querySelector('#menu-publish-btn') ? changeMenu() : null));
 }
 
 window.onload = function () {
     console.log("starting on load")
-    // loadFile('../dist/content/about/intro.txt',body.children.item(1).children.item(1));
     // changeCurrentContent(pages.Home);
     cocoResize();
-    getFile('../dist/cocodata.json');
-    document.querySelector('#menu-publish-btn').addEventListener('click', () => addNewContent());
-    document.querySelectorAll('.dropdown-item')
-        .forEach(item=>item.addEventListener('click',()=> item!==document.querySelector('#menu-publish-btn')?changeMenu():null));
+    if (!contentVault) loadCocoData();
+    setMenuListeners();
 
 }
-
 
 
 function cocoResize() {
@@ -197,17 +200,20 @@ function cocoResize() {
 //----------------------------------------------------------------------------------------------------------------------
 
 function createForm() {
+    let saveBtn = document.querySelector('#btn-save');
+    saveBtn.disabled = !contentVault.unsavedData;
+    saveBtn.addEventListener('click', () => saveAllChanges());
     document.querySelector("#section-select").addEventListener("change", () => checkSelector());
     document.querySelector("#type-select").addEventListener("change", () => checkType());
     document.querySelector("#btn-generate").disabled = true;
-    document.querySelector("#btn-cancel").addEventListener("click",()=>closeEditMode())
+    document.querySelector("#btn-cancel").addEventListener("click", () => closeEditMode())
     document.querySelector('#btn-process').disabled = true;
     document.querySelector('#btn-process').addEventListener("click", () => processContent())
     console.log(document.querySelector("#btn-generate").disabled)
 
 }
 
-let prevContent;
+let newContent;
 
 function checkAllInputs() {
     let d = document, [inputs, btnProcess] = [
@@ -225,6 +231,17 @@ function checkAllInputs() {
 function checkType() {
     const value = document.querySelector('#type-select').value;
     let formContentBox = document.querySelector('#content-box');
+
+    function loadContentList() {
+        let pagename = PAGES[document.querySelector('#select-section-link').value].name;
+        console.log('LOADING SECTION LIST: ' + pagename);
+        if (contentVault) {
+            let section = contentVault.getSection(pagename);
+            let contSelect = document.querySelector('#select-content-link');
+            section.forEach(elem => contSelect.options[contSelect.options.length] = new Option(elem.title, elem.id));
+        }
+    }
+
     if (value == 1) {
         removeAllChildNodes(formContentBox);
         formContentBox.insertAdjacentHTML('afterbegin', `
@@ -233,12 +250,12 @@ function checkType() {
             <div class="form-outline row m-2 ">
             <textarea id="card-content" class="form-control col-sm-10 m-2" rows="10" cols="100"
                       placeholder="CONTENIDO (admite código HTML)"></textarea>
-                      <div id="content-current-buttons"class="row no-wrap m-2">
-                      <div id="content-current-pictures"class="row no-wrap m-2">
+                      <div id="content-current-buttons" class="row no-wrap m-2">
+                      <div id="content-current-pictures" class="row no-wrap m-2">
                    
             <div id="content-buttons" class="row no-wrap m-2">
-                <div id="add_btn_btn" class="btn-primary p-3 m-3 ">añadir botón</div>
-                <div id="add_pic_btn" class="btn-primary p-3 m-3 ">añadir imagen</div>
+                <button id="add_btn_btn" class="btn-primary p-3 m-3 ">añadir botón</button>
+                <button id="add_pic_btn" class="btn-primary p-3 m-3 ">añadir imagen</button>
             </div>
         </div>`);
     } else if (value == 2) {
@@ -249,12 +266,36 @@ function checkType() {
             <input type="text" id="card-action" class="form-control col-sm-10 m-2"
                    placeholder="Introducir la URL de destino"/>
         </div>`);
+    } else if (value == 3) {
+        let editOptions = document.querySelector('#editOptions');
+        removeAllChildNodes(editOptions);
+        editOptions.insertAdjacentHTML('afterbegin', `
+        <p class="label row no-wrap m-2" >Destination</p>
+        <div class="form-outline row no-wrap m-2">
+            <select  id="select-section-link" class="form-control sm-10 m-3">
+                <option selected value="-1"> elige...</option>
+                <option value="0">Home</option>
+                <option value="1">Comunicados</option>
+                <option value="2">Documentos</option>
+                <option value="3">Magazines</option>
+                <option value="4">Pildoras</option>
+                <option value="5">Plantillas</option>
+                <option value="6">Apps</option>
+                <option value="7">Sobre nosotros</option>
+            </select>
+             <select  id="select-content-link" class="form-control sm-10 m-3">
+                <option selected value="0"> elige...</option>
+            </select>
+        
+        </div>`);
+
+        document.querySelector('#select-section-link').addEventListener('change', () => loadContentList());
     }
-    expandOptions(document.querySelector('#section-select').value > 0 && value > 0);
+    expandOptions(document.querySelector('#section-select').value >= 0 && value > 0);
 }
 
 function checkSelector() {
-    expandOptions(document.querySelector('#section-select').value > 0
+    expandOptions(document.querySelector('#section-select').value >= 0
         && document.querySelector('#type-select').value > 0);
 }
 
@@ -262,14 +303,47 @@ function expandOptions(show) {
     $('#editOptions').collapse(show ? 'show' : 'hide');
 }
 
-function saveCard() {
-    return download(JSON.stringify(prevContent, null, '\t'), 'prevContent')
+function updateMainBody() {
+
+}
+
+function saveNewCard() {
+    let section=PAGES[document.querySelector('#section-select').value].name;
+    
+    contentVault.addContent(newContent,section);
+    newContent=null;
+    contentVault.unsavedData=true;
+    emptyForm();
+    removeAllChildNodes(document.querySelector('#preview'));
+    removeAllChildNodes(document.querySelector('#main_body'));
+    contentVault.addAllSectionContent(document.querySelector('#main_body'),section)
+    //todo: change to contentVault adding and delete newContent
+    // return download(JSON.stringify(newContent, null, '\t'), 'newContent')
+}
+
+function emptyForm() {
+    if (newContent) {
+        //TODO CONFIRMATION MODAL UNSAVED DATA
+    }
+    closeEditMode();
+    openEditMode();
+
+    //EMPTY ALL FIELDS
+}
+
+function fillForm(content) {
+    //TODO fill all fields with existing content
+}
+
+function saveAllChanges() {
+    contentVault.saveContentData();
+    //TODO Download json
 }
 
 function processContent() {
 
     let value = document.querySelector("#type-select").value;
-    prevContent = new Card(
+    newContent = new Card(
         document.querySelector("#card-title").value,
         document.querySelector("#card-description").value,
         value == 1 ? document.querySelector("#card-content").value : null,
@@ -279,19 +353,25 @@ function processContent() {
 
     let preview = document.querySelector('#preview');
     removeAllChildNodes(preview);
-    preview.insertAdjacentHTML("beforeend", prevContent.html);
-    prevContent.activate(preview.lastChild);
+    preview.insertAdjacentHTML("beforeend", newContent.getHtml());
+    
+    newContent.activate();
     document.querySelector("#btn-generate").disabled = false;
-    document.querySelector("#btn-generate").addEventListener('click', () => saveCard());
+    document.querySelector("#btn-generate").addEventListener('click', () => saveNewCard());
 }
 
-// window.onload= function (){
-// }
+
+//----------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
+//------------------------------------------------------UTILS-----------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------------------------
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
 }
+
 function download(content, fileName) {
     var a = document.createElement("a");
     var file = new Blob([content], {type: "application/json"});
--- dist/js/content.js
+++ dist/js/content.js
@@ -1,149 +1,199 @@
-const pageNames= Object.freeze({
-    Home: Symbol("home"|1),
-    Comunicados: Symbol("comunicados"|2),
-    Documentos: Symbol("documentos"|3),
-    Magazines: Symbol("magazines"|4),
-    Pildoras: Symbol("pildoras"|5),
-    Plantillas: Symbol("plantillas"|6),
-    Tools: Symbol("tools"|7),
-    About: Symbol("about"|8)
-})
+export const PAGES= Object.freeze([
+    {name:"home",num:0},
+    {name:"comunicados",num:1},
+    {name:"documentos",num:2},
+    {name:"magazines",num:3},
+    {name:"pildoras",num:4},
+    {name:"plantillas",num:5},
+    {name:"tools",num:6},
+    {name:"about",num:7}
+])
 
 function constructPage(pageName) {
 
 }
+//from https://www.xul.fr/javascript/map-and-object.php
+    function mapToObject(m) {
+        let lo = {}
+        for (let [k, v] of m) {
+            if (v instanceof Map) {
+                lo[k] = mapToObject(v)
+            } else {
+                lo[k] = v
+            }
+        }
+        return lo
+    }
+
+    function objectToMap(o) {
+        let m = new Map()
+        for (let k of Object.keys(o)) {
+            if (o[k] instanceof Object) {
+                m.set(k, objectToMap(o[k]))
+            } else {
+                m.set(k, o[k])
+            }
+        }
+        return m
+    }
 
 export class ContentLoader {
     pages
+    lastUpdate
+    unsavedData
     constructor(json) {
         if(json==null) {
             this.pages= new Map();
-            console.log(pageNames)
-            this.pages.set(pageNames.Home.description,[])
-            this.pages.set(pageNames.Comunicados.description,[])
-            this.pages.set(pageNames.Documentos.description,[])
-            this.pages.set(pageNames.Magazines.description,[])
-            this.pages.set(pageNames.Pildoras.description,[])
-            this.pages.set(pageNames.Plantillas.description,[])
-            this.pages.set(pageNames.Tools.description,[])
-            this.pages.set(pageNames.About.description,[])
-            download(JSON.stringify(Object.fromEntries(this.pages)),'cocodata')
-            // pageNames.forEach(name=>this.pages.set(name.description,null));
-
+            PAGES.forEach(page=> this.pages.set(page.name,new Map()));
+            this.updateDate();
+            console.log(this);
+            this.unsavedData=true;
         }else{
-            this.pages = new Map(Object.entries(JSON.parse(json)));
+            this.pages = objectToMap(JSON.parse(json));
+            this.unsavedData=false;
         }
     }
-    addContent(card,section){
-        let secSection=  Number.isNumber(section)?pageNames.get(section).description:section;
-        let val = this.pages.get(secSection);
-        val.push(card);
-        this.pages.set(secSection,val);
-    }
-    removeContent(card){
-        this.pages.remove(card);
+
+    updateDate() {
+        let da= new Date();
+        this.lastUpdate = da.getFullYear() * 10000 + da.getMonth() * 100 + da.getDay();
     }
 
+    saveContentData() {
+        this.updateDate();
+        this.unsavedData=false;
+        download(JSON.stringify(mapToObject(this.pages)), 'cocodata');
+    }
+
+    addContent(card,section){
+        if(!isNaN(section))section=PAGES[section].name;
+        this.unsavedData=true;
+        this.updateDate()
+        card.date=new Date();
+        console.log(section)
+        this.pages.get(section).set(card.id,card);
+    }
+    removeContent(card){
+        this.unsavedData=true;
+        this.pages.remove(card);
+    }
+    getSection(section){
+        console.log('Get section: '+section);
+        if(!isNaN(section))section=PAGES[section];
+        return this.pages.get(section);
+
+    }
+    getContent(section,id){
+        //todo
+    }
+
+    addAllSectionContent(target,section){
+        let list = Array.from(this.getSection(section).values());
+        list= list.sort((a,b) => (a.date > b.date) ? 1 : ((b.date > a.date) ? -1 : 0));
+        for (let i = 0; i < list.length; i++) {
+            console.log(list[i])
+            target.insertAdjacentHTML('beforeend',list[i].getHtml())
+            list[i].activate();
+        }
+
+
+    }
 }
-// import { saveAs } from 'file-saver';
 export class Card {
-    html;
-    expandedHtml;
-    element;
     title;
     card_description;
     article;
     img_src;
     url;
     id;
+    date;
     constructor(title, card_description, article, img_src,url) {
         console.log(article)
         this.title = title.trim();
         this.card_description = card_description.trim();
         this.article = article==null?null:article;
+        this.id=title.trim().replace(/[^0-9a-zA-Z_ ]/g, "").replace(' ',"_").toLowerCase();
 
         if(url!=null){
             url=url.trim();
             this.url = url.includes('./')?url:url.includes('http')?url.trim():'https://'+url.trim();
         }
-        this.img_src = img_src.includes("/")?img_src.trim():'../img/'+img_src.trim();
+        this.img_src = img_src.includes("/")?img_src.trim():'./dist/img/'+img_src.trim();
         this.title=title;
-        this.html=this.constructHtml(
-            this.title,
-            this.img_src,
-            this.card_description,
-            this.url==null?null:this.url);
+        Object.defineProperty(this, 'expanded', {value: 'static', writable: true});
+        this.expanded=false;
+        this.constructHtml();
+        console.log('HTML: '+ this.html)
+        console.log('JSON: ')
+        console.log(JSON.stringify(this))
     }
-    activate(element) {
-        let auxId= '#'+this.id
+    activate() {
+        let auxId= (this.expanded?'#ex_card_':'#card_')+this.id
+        console.log('activating: '+auxId)
         document.querySelector(auxId).addEventListener("click", this.url == null ? () => this.expand() : () => this.openLink())
     }
     expand(){
-        console.log("EXPANDING")
-        let auxId = '#' + this.id;
+        console.log("EXPANDING: "+this.id)
+        let auxId= (this.expanded?'#ex_card_':'#card_')+this.id;
         let oldElem = document.querySelector(auxId);
-        if(!this.id.includes('ex_')) {
+        if(!this.expanded) {
+            this.expanded=true;
             oldElem.parentElement.insertAdjacentHTML("afterbegin",
-                this.expandedHtml==null?
-                this.constructExpandedHtml(
-                    this.title,
-                    this.img_src,
-                    this.article,
-                ) : this.expandedHtml
-            )
-            auxId = '#' + this.id;
+                this.getHtml()
+            );
+            auxId = '#ex_card_' + this.id;
             oldElem.remove();
-            let newElem=document.querySelector(auxId);
-            this.activate(newElem)
-            this.element=newElem;
+            this.activate()
             return document.querySelector(auxId)
         }
-        oldElem.parentElement.insertAdjacentHTML("afterbegin",this.html)
-        this.id=this.id.replace('ex_','');
-        auxId = '#' + this.id;
+        this.expanded=false;
+        oldElem.parentElement.insertAdjacentHTML("afterbegin",this.getHtml())
+        auxId = '#card_' + this.id;
         oldElem.remove();
-        let newElem=document.querySelector(auxId);
-        this.activate(newElem)
-        this.element=newElem;
+        this.activate()
         return document.querySelector(auxId)
     }
-
-
     openLink(){
         window.open(this.url, '_blank').focus();
     }
-
-    constructHtml(title, img_src,description,url){
-        let id= 'card_'+title.trim().replace(/[^a-zA-Z ]/g, "").replace(' ',"_").toLowerCase();
-        this.id=id;
-        const urlTag= url==null?'':'<div class="btn btn-primary">Abrir</a>\n'
-        return `<div id="${id}"class="card">
+    constructHtml(){
+        Object.defineProperty(this, 'html', {value: 'static', writable: true});
+        let auxId= 'card_'+ this.id;
+        const urlTag= this.url==null?'':'<div class="btn btn-primary">Abrir</a>\n'
+        return this.html=`<div id="${auxId}"class="card">
                     <div class="card-header">
-                        <img class="card-img-top" src="${img_src}" alt="Card image cap">
+                        <img class="card-img-top" src="${this.img_src}" alt="Card image cap">
                     </div>
                     <div class="card-body">
-                          <h5 class="card-title">${title}</h5>
-                          <p class="card-text">${description}</p>  
+                          <h5 class="card-title">${this.title}</h5>
+                          <p class="card-text">${this.card_description}</p>  
                           ${urlTag}
                     </div>
                </div>`
     }
-    constructExpandedHtml(title, img_src,content){
-        let id= 'ex_card_'+(title.trim().replace(/[^a-zA-Z ]/g, "")
-            .replace(' ',"_").toLowerCase().replace('ex',''));
-        this.id=id;
-        return `<div id="${id}"class="card expanded">
+    constructExpandedHtml(){
+        Object.defineProperty(this, 'expandedHtml', {value: 'static', writable: true});
+
+        let auxId= 'ex_card_'+this.id;
+        return this.expandedHtml=`<div id="${auxId}"class="card expanded">
                     <div class="card-header expanded">
-                        <img class="card-img-top expanded" id="${id}_img" src="${img_src}" alt="Card image cap">
-                        <h5 class="card-title">${title}</h5>
+                        <img class="card-img-top expanded" id="${auxId}_img" src="${this.img_src}" alt="Card image cap">
+                        <h5 class="card-title">${this.title}</h5>
                     </div>
                     <div class="card-body expanded">
-                          <div class="card-text expanded">${content}</div>  
+                          <div class="card-text expanded">${this.article}</div>  
                     </div>
                </div>`
     }
 
-
+    getHtml(){
+        if(this.expanded){
+            if (this.expandedHtml)return this.expandedHtml;
+            return this.constructExpandedHtml();
+        }
+        if (this.html) return this.html;
+        return this.constructHtml();
+    }
 }
 function download(content, fileName) {
     var a = document.createElement("a");
